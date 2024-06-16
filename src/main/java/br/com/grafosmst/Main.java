c0048275e5fafbf363b7ca2eaf7090b088adb864
Fernando Vaz <fertwbr@gmail.com>
2024-06-16 19:28:03 -0300

    feat: Main Class and Input/Output

--- src/main/java/br/com/grafosmst/Main.java
+++ src/main/java/br/com/grafosmst/Main.java
@@ -1,17 +1,47 @@
 package br.com.grafosmst;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.grafosmst.algorithm.Kruskal;
+import br.com.grafosmst.algorithm.Prim;
+import br.com.grafosmst.model.Aresta;
+import br.com.grafosmst.model.Grafo;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Número de vértices: ");
+        int vertices = scanner.nextInt();
+        Grafo grafo = new Grafo(vertices);
+
+        System.out.print("Número de arestas: ");
+        int arestas = scanner.nextInt();
+
+        System.out.println("Digite as arestas (origem destino peso):");
+        for (int i = 0; i < arestas; i++) {
+            int origem = scanner.nextInt();
+            int destino = scanner.nextInt();
+            int peso = scanner.nextInt();
+            grafo.adicionarAresta(origem, destino, peso);
         }
+
+        Prim prim = new Prim();
+        List<Aresta> mstPrim = prim.executarPrim(grafo);
+        System.out.println("\nÁrvore Geradora Mínima (Prim):");
+        imprimirMST(mstPrim);
+
+        Kruskal kruskal = new Kruskal();
+        List<Aresta> mstKruskal = kruskal.executarKruskal(grafo);
+        System.out.println("\nÁrvore Geradora Mínima (Kruskal):");
+        imprimirMST(mstKruskal);
+    }
+
+    private static void imprimirMST(List<Aresta> mst) {
+        int pesoTotal = 0;
+        for (Aresta aresta : mst) {
+            System.out.println(aresta.getOrigem() + " - " + aresta.getDestino() + " : " + aresta.getPeso());
+            pesoTotal += aresta.getPeso();
+        }
+        System.out.println("Peso total da MST: " + pesoTotal);
     }
 }

4bcbf6244c9dc68a455a84cdaa7e23b4f48df245
Fernando Vaz <fertwbr@gmail.com>
2024-06-16 19:22:26 -0300

    feat: adding getters in grafo

--- src/main/java/br/com/grafosmst/model/Grafo.java
+++ src/main/java/br/com/grafosmst/model/Grafo.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Grafo {
     int vertices;
     List<Vertice> verticesList;
+    List<Aresta> arestas;
 
     public Grafo(int vertices) {
         this.vertices = vertices;
         this.verticesList = new ArrayList<>();
+        this.arestas = new ArrayList<>();
         for (int i = 0; i < vertices; i++) {
             verticesList.add(new Vertice(i));
         }
@@ -19,6 +21,7 @@ public class Grafo {
         Aresta aresta = new Aresta(origem, destino, peso);
         verticesList.get(origem).adicionarAresta(aresta); // Adiciona a aresta ao vértice de origem
         verticesList.get(destino).adicionarAresta(aresta); // Adiciona a aresta ao vértice de destino
+        arestas.add(aresta);
     }
 
     public int getVertices() {
@@ -27,5 +30,9 @@ public class Grafo {
 
     public List<Vertice> getVerticesList() {
         return verticesList;
+    }
+
+    public List<Aresta> getArestas() {
+        return arestas;
     }
 }
